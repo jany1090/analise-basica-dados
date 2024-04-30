9d8ebac130469097c1fc9676c3eac0bc1388fadb
jany1090 <jany1090@hotmail.com>
2024-04-30 16:08:21 -0300

    Aula de Deploy remoto de Banco de Dados e Triggers

---
+++ aula1504/script1504.sql
@@ -0,0 +1,15 @@
+/*Extração com SubQuery*/
+use empregados_db;
+select * from empregado where cod_depto in(
+	select cod_depto from departamento where cod_depto % 2 - 0
+);
+select * from empregado where cod_depto in(
+	select cod_depto from departamento where cod_depto < 5
+);
+
+/*Calculo com Datas*/
+select *, floor(datediff(now(), dt_nascimento) / 365) as idade from dependente;
+
+select * from empregado where cod_emp in(
+	select cod_emp from dependente where floor(datediff(now(), dt_nascimento) / 365) < 18
+);
